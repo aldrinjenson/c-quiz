8d91579c13512219414ce53d36035c74f65e3a1d
Aldrin Jenson <aldrinjenson@gmail.com>
2022-08-25 21:22:11 +0530

    minor improvments✨

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <fstream>
 #include <iostream>
 #include <nlohmann/json.hpp>
+#define MAX_LIVES 5
 
 using json = nlohmann::json;
 using namespace std;
@@ -8,20 +9,23 @@ using namespace std;
 int main() {
   std::ifstream f("data.json");
   json questions = json::parse(f);
+  int qCounter = 0, lives = MAX_LIVES, score = 0, choice;
+  string playerName;
 
-  int qCounter = 0;
-  int lives = 5;
-  int score = 0;
-  int choice;
-  for (qCounter = 0; qCounter < questions.size(); qCounter++) {
+  cout << "Enter your name: ";
+  cin >> playerName;
+  cout << "Hello " << playerName << ", Welcome to cQuiz!\n\n";
+  for (qCounter = 0; qCounter < questions.size() && lives > 0; qCounter++) {
     json currQues = questions[qCounter];
     json options = currQues["options"];
-    cout << "\n\n" << qCounter + 1 << "). " << currQues["q"] << endl;
+
+    cout << "\nQ" << qCounter + 1 << ". " << currQues["q"] << endl;
     cout << "Options:\n";
     for (int i = 0; i < options.size(); i++) {
       auto op = options[i];
       cout << i + 1 << ") " << op << endl;
     }
+
   getOptionInput:
     cout << "\nEnter your choice: ";
     cin >> choice;
@@ -29,8 +33,10 @@ int main() {
       cout << "Invalid option!\nPlease try again";
       goto getOptionInput;
     }
+
     auto correctAns = currQues["a"];
     auto chosenAns = options[choice - 1];
+
     if (chosenAns == correctAns) {
       score += 5;
       cout << "Right Answer!\nScore = " << score << endl;
@@ -39,4 +45,9 @@ int main() {
       cout << "\nLives left = " << --lives;
     }
   }
+
+  cout << "\n\nGame over!\n\tStatus report of " << playerName
+       << "\nQuestions solved = " << qCounter
+       << "\nLives used = " << MAX_LIVES - lives << "\nFinal Score = " << score
+       << endl;
 }
