9953b214561fa1a60b4eb82224b2c8667b370e87
Aldrin Jenson <aldrinjenson@gmail.com>
2022-08-28 01:46:53 +0530

    show high scores

--- main.cpp
+++ main.cpp
@@ -70,8 +70,24 @@ getInput:
   return chosenCategory;
 }
 
-void saveData(std::shared_ptr<sql::PreparedStatement> &stmnt,
-              sql::SQLString playerName, int score) {
+sql::Driver *driver = sql::mariadb::get_driver_instance();
+sql::SQLString url("jdbc:mariadb://localhost:3306/test");
+sql::Properties properties({{"user", "db_user"},
+                            {"password", "db_user_password"}});
+std::unique_ptr<sql::Connection> conn(driver->connect(url, properties));
+
+void addScoreToDb(string initialName, int score) {
+  string playerName = initialName;
+  printf("Enter name to be saved(default = %s, enter x to skip save): ",
+         initialName.c_str());
+  cin >> playerName;
+  if (playerName == "x") {
+    cout << "Skipping..\n";
+    exit(0);
+  }
+  std::shared_ptr<sql::PreparedStatement> stmnt(
+      conn->prepareStatement("INSERT INTO test.quizscores(name, "
+                             "score) VALUES (?, ?)"));
   try {
     stmnt->setString(1, playerName);
     stmnt->setInt(2, score);
@@ -82,30 +98,27 @@ void saveData(std::shared_ptr<sql::PreparedStatement> &stmnt,
               << std::endl;
   }
 }
-sql::Driver *driver = sql::mariadb::get_driver_instance();
-sql::SQLString url("jdbc:mariadb://localhost:3306/test");
-sql::Properties properties({{"user", "db_user"},
-                            {"password", "db_user_password"}});
-std::unique_ptr<sql::Connection> conn(driver->connect(url, properties));
 
-void addScoreToDb(string initialName, int score) {
-  string playerName = initialName;
-  printf("Enter name to be saved(default = %s): ", initialName.c_str());
-  cin >> playerName;
-  std::shared_ptr<sql::PreparedStatement> stmnt(
-      conn->prepareStatement("INSERT INTO test.quizscores(name, "
-                             "score) VALUES (?, ?)"));
-  saveData(stmnt, playerName, score);
-}
 void showHighScores() {
-  std::shared_ptr<sql::PreparedStatement> stmnt(
-      conn->prepareStatement("SELECT * from test.quizscores"));
+  shared_ptr<sql::Statement> stmnt(conn->createStatement());
+  int counter = 1;
+  cout << "All time High scores:\n";
+  try {
+    std::unique_ptr<sql::ResultSet> res(stmnt->executeQuery(
+        "SELECT name, score FROM test.quizscores order by score desc"));
+    while (res->next()) {
+      std::cout << counter++ << ". " << res->getString("name") << ": "
+                << res->getString("score") << endl;
+    }
+  } catch (sql::SQLException &e) {
+    std::cerr << "Error printing scores: " << e.what() << std::endl;
+    exit(0);
+  }
 }
 
 int main() {
   string playerName;
   cout << "Enter your name: ";
-
   cin >> playerName;
   cout << "Hello " << playerName << ", Welcome to cQuiz!\n\n";
   cout << "Loading categories available...\n";
@@ -162,5 +175,6 @@ int main() {
        << "\nLives used = " << MAX_LIVES - lives << "\nFinal Score = " << score
        << endl;
   addScoreToDb(playerName, score);
+  showHighScores();
   conn->close();
 }
